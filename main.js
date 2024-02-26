5c9b70e98dadad21929799e54ebb2ca1e85ff796
abhishek-sharma-2001 <abhishek.sharma.12.10.2001@gmail.com>
2024-02-26 11:24:10 +0530

    updated file name

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ renderer.render(scene, camera);
 
 const fontLoader = new FontLoader();
 let font;
-fontLoader.load('./SpaceFrigate_Regular.json', loadedFont => {
+fontLoader.load('./font.json', loadedFont => {
   font = loadedFont;
   console.log("Font loaded:", font);
 
